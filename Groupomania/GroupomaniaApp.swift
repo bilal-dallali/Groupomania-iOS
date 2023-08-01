f8a856c060dc1d4fd90bdf12b2bcb41b56a178bc
bilal-dallali <dallali-bilal@hotmail.fr>
2023-08-01 15:39:37 +0200

    Initial Commit

---
+++ Groupomania/GroupomaniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupomaniaApp.swift
+//  Groupomania
+//
+//  Created by Bilal Dallali on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroupomaniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
